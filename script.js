026321707738a186d77657a59e7e8472d46a3380
cunhavcs <cunha.vcs@gmail.com>
2022-09-11 16:42:54 -0300

    Resolve requisito 6

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function geraCorAleatoria() {
   const caracteresPossiveis = '0123456789ABCDEF';
   let cor = '#';
-  
+
   for (let index = 0; index < 6; index += 1) {
     const novoCaractere = caracteresPossiveis[Math.floor(Math.random() * 16)];
     cor += novoCaractere;
   }
-  
+
   return cor;
+}
+
+function gravaUltimaPaleta(value) {
+  localStorage.setItem('colorPalette', JSON.stringify(value));
 }
 
 function geraNovaPaleta() {
   const caixaDeCor = document.getElementsByClassName('color');
-  let paleta = [];
+  const paleta = [];
 
   for (let index = 1; index < 4; index += 1) {
     const caixaAtual = caixaDeCor[index];
@@ -26,10 +30,6 @@ function geraNovaPaleta() {
 
 const botaoCoresAleatorias = document.getElementById('button-random-color');
 botaoCoresAleatorias.addEventListener('click', geraNovaPaleta);
-
-function gravaUltimaPaleta(value) {
-  localStorage.setItem('colorPalette', JSON.stringify(value));
-}
 
 function recuperaUltimaPaleta() {
   const ultimaPaleta = JSON.parse(localStorage.getItem('colorPalette'));
