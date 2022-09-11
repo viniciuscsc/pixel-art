4aaad20ce93fce639d6a297de5f51c946dfa065f
cunhavcs <cunha.vcs@gmail.com>
2022-09-11 18:55:37 -0300

    Resolve requisitos 9 e 10

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+const body = document.querySelector('body');
+
+const h1 = document.createElement('h1');
+h1.id = 'title';
+h1.innerText = 'Paleta de Cores';
+body.appendChild(h1);
+
+const paletaDeCor = document.createElement('section');
+paletaDeCor.id = 'color-palette';
+body.appendChild(paletaDeCor);
+
+for (let index = 0; index < 4; index += 1) {
+  const divCor = document.createElement('div');
+  divCor.className = 'color';
+  divCor.style.border = 'black solid 1px';
+  divCor.style.display = 'inline-block';
+  divCor.style.height = '50px';
+  divCor.style.width = '50px';
+  divCor.style.marginRight = '5px';
+  switch (index) {
+    case 0:
+      divCor.className = 'color selected';
+      divCor.style.backgroundColor = 'black';
+      break;
+    case 1:
+      divCor.style.backgroundColor = 'red';
+      break;
+    case 2:
+      divCor.style.backgroundColor = 'blue';
+      break;
+    case 3:
+      divCor.style.backgroundColor = 'green';
+      break;
+  }
+  body.appendChild(divCor);
+}
+
+const botaoCoresAleatorias = document.createElement('button');
+botaoCoresAleatorias.id = 'button-random-color';
+botaoCoresAleatorias.innerText = 'Cores aleatórias';
+botaoCoresAleatorias.style.display = 'block';
+botaoCoresAleatorias.style.marginTop = '15px';
+botaoCoresAleatorias.style.marginBottom = '30px';
+body.appendChild(botaoCoresAleatorias);
+
+const quadroDePixels = document.createElement('section');
+quadroDePixels.id = 'pixel-board';
+quadroDePixels.style.height = '210px';
+quadroDePixels.style.width = '210px';
+quadroDePixels.style.display = 'flex';
+quadroDePixels.style.flexWrap = 'wrap';
+body.appendChild(quadroDePixels);
+
+for (let index = 0; index < 25; index += 1) {
+  const divPixel = document.createElement('div');
+  divPixel.className = 'pixel';
+  divPixel.style.height = '40px';
+  divPixel.style.width = '40px';
+  divPixel.style.border = 'black solid 1px';
+  divPixel.style.backgroundColor = 'white';
+  quadroDePixels.appendChild(divPixel);
+}
+
 function geraCorAleatoria() {
   const caracteresPossiveis = '0123456789ABCDEF';
   let cor = '#';
@@ -28,7 +91,6 @@ function geraNovaPaleta() {
   gravaUltimaPaleta(paleta);
 }
 
-const botaoCoresAleatorias = document.getElementById('button-random-color');
 botaoCoresAleatorias.addEventListener('click', geraNovaPaleta);
 
 function recuperaUltimaPaleta() {
@@ -46,7 +108,7 @@ if (localStorage.getItem('colorPalette') !== null) {
   recuperaUltimaPaleta();
 }
 
-function selecionaCor(event) {
+function selecionaCor() {
   const caixaDeCor = document.getElementsByClassName('color');
 
   for (let index = 0; index < 4; index += 1) {
@@ -54,8 +116,7 @@ function selecionaCor(event) {
     caixaAtual.className = 'color';
   }
 
-  event.target.className = 'color selected';
-  
+  event.target.className = 'color selected';  
 }
 
 const caixaDeCor = document.getElementsByClassName('color');
@@ -63,4 +124,23 @@ const caixaDeCor = document.getElementsByClassName('color');
 for (let index = 0; index < 4; index += 1) {
   const caixaAtual = caixaDeCor[index];
   caixaAtual.addEventListener('click', selecionaCor);
+}
+
+function pintaPixel() {
+  const caixaDeCor = document.getElementsByClassName('color');
+
+  for (let index = 0; index < 4; index += 1) {
+    const caixaAtual = caixaDeCor[index];
+    
+    if(caixaAtual.className.includes('selected')) {
+      event.target.style.backgroundColor = caixaAtual.style.backgroundColor;
+    }
+  }
+}
+
+const quadroPixels = document.getElementsByClassName('pixel');
+
+for (let index = 0; index < quadroPixels.length; index += 1) {
+  const pixelAtual = quadroPixels[index];
+  pixelAtual.addEventListener('click', pintaPixel);
 }
