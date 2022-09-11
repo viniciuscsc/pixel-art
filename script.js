e11cac88f74617b91b67f704e4c0293d61321d0a
cunhavcs <cunha.vcs@gmail.com>
2022-09-11 12:08:35 -0300

    Resolve requisito 5

--- script.js
+++ script.js
@@ -2,16 +2,27 @@ function randomColors() {
     let red = parseInt((Math.random()) * 255);
     let green = parseInt((Math.random()) * 255);
     let blue = parseInt((Math.random()) * 255);
-    let color = "rgb(" + red + ", " + green + ", " + blue + ")";
+    let color = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+    
     return color;
 }
 
 function updatePaletteColors() {
-    let randomColor = document.getElementsByClassName("color");
+    let randomColor = document.getElementsByClassName('color');
+    
     for (let index = 1; index < randomColor.length; index += 1) {
         randomColor[index].style.backgroundColor = randomColors();
-    }
+        
+        currentKey = 'color' + index;
+        currentValue = randomColor[index].style.backgroundColor;
+
+        savePaletteColors(currentKey, currentValue);
+    }    
 }
 
-const button = document.getElementById("button-random-color");
-button.addEventListener("click", updatePaletteColors);
+function savePaletteColors(key, value) {
+    localStorage.setItem(key, value);
+}
+
+const button = document.getElementById('button-random-color');
+button.addEventListener('click', updatePaletteColors);
