88f776afff906561b2c228191f2543750afa013f
cunhavcs <cunha.vcs@gmail.com>
2022-09-11 19:24:40 -0300

    Resolve requisitos 9 e 10

--- script.js
+++ script.js
@@ -1,65 +1,8 @@
-const body = document.querySelector('body');
-
-const h1 = document.createElement('h1');
-h1.id = 'title';
-h1.innerText = 'Paleta de Cores';
-body.appendChild(h1);
-
-const paletaDeCor = document.createElement('section');
-paletaDeCor.id = 'color-palette';
-body.appendChild(paletaDeCor);
-
-for (let index = 0; index < 4; index += 1) {
-  const divCor = document.createElement('div');
-  divCor.className = 'color';
-  divCor.style.border = 'black solid 1px';
-  divCor.style.display = 'inline-block';
-  divCor.style.height = '50px';
-  divCor.style.width = '50px';
-  divCor.style.marginRight = '5px';
-  switch (index) {
-    case 0:
-      divCor.className = 'color selected';
-      divCor.style.backgroundColor = 'black';
-      break;
-    case 1:
-      divCor.style.backgroundColor = 'red';
-      break;
-    case 2:
-      divCor.style.backgroundColor = 'blue';
-      break;
-    case 3:
-      divCor.style.backgroundColor = 'green';
-      break;
-  }
-  body.appendChild(divCor);
-}
-
-const botaoCoresAleatorias = document.createElement('button');
-botaoCoresAleatorias.id = 'button-random-color';
-botaoCoresAleatorias.innerText = 'Cores aleatórias';
-botaoCoresAleatorias.style.display = 'block';
-botaoCoresAleatorias.style.marginTop = '15px';
-botaoCoresAleatorias.style.marginBottom = '30px';
-body.appendChild(botaoCoresAleatorias);
-
-const quadroDePixels = document.createElement('section');
-quadroDePixels.id = 'pixel-board';
-quadroDePixels.style.height = '210px';
-quadroDePixels.style.width = '210px';
-quadroDePixels.style.display = 'flex';
-quadroDePixels.style.flexWrap = 'wrap';
-body.appendChild(quadroDePixels);
-
-for (let index = 0; index < 25; index += 1) {
-  const divPixel = document.createElement('div');
-  divPixel.className = 'pixel';
-  divPixel.style.height = '40px';
-  divPixel.style.width = '40px';
-  divPixel.style.border = 'black solid 1px';
-  divPixel.style.backgroundColor = 'white';
-  quadroDePixels.appendChild(divPixel);
-}
+const coresIniciais = document.getElementsByClassName('color');
+coresIniciais[0].style.backgroundColor = 'black';
+coresIniciais[1].style.backgroundColor = 'red';
+coresIniciais[2].style.backgroundColor = 'blue';
+coresIniciais[3].style.backgroundColor = 'green';
 
 function geraCorAleatoria() {
   const caracteresPossiveis = '0123456789ABCDEF';
@@ -91,6 +34,7 @@ function geraNovaPaleta() {
   gravaUltimaPaleta(paleta);
 }
 
+const botaoCoresAleatorias = document.getElementById('button-random-color');
 botaoCoresAleatorias.addEventListener('click', geraNovaPaleta);
 
 function recuperaUltimaPaleta() {
@@ -132,7 +76,7 @@ function pintaPixel() {
   for (let index = 0; index < 4; index += 1) {
     const caixaAtual = caixaDeCor[index];
     
-    if(caixaAtual.className.includes('selected')) {
+    if (caixaAtual.className.includes('selected')) {
       event.target.style.backgroundColor = caixaAtual.style.backgroundColor;
     }
   }
